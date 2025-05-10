e8db8dbc1825d38f5a808f4dcda545a7807249cd
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-10 09:28:31 +0700

    KaiO

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -19,7 +19,6 @@ from django.conf.urls import include
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from api.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
