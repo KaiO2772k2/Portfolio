6b08640f45b8934b64c85c43547f3b373b459664
Kai.O <92730257+KaiO277@users.noreply.github.com>
2025-05-17 09:26:46 +0700

    Nghia

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Project, Language
+from .models import Project, Language, ProjectDetail
 
 # Register your models here.
 
 admin.site.register(Project)
 admin.site.register(Language)
+admin.site.register(ProjectDetail)
